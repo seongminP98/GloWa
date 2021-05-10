fa6f75d838887a697e52d633c496f5bb3091bf90
SunEom <dkxl0701@gmail.com>
2021-05-10 17:48:34 +0900

    React configure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Text = styled.div`
+  font-size: 30px;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Text>Hello</Text>
     </div>
   );
 }
