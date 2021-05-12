973d4213cece1758a281bad121fb5a57a8e3e265
seongmin <psm7335@gmail.com>
2021-05-12 18:05:41 +0900

    login & Sign Up

---
+++ server/routes/auth.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const passport = require('passport');
+const router = express.Router();
+
+router.post('/login',(req, res, next)=>{
+    passport.authenticate('local',(err, user, info)=>{
+        if(err){
+            return next(err);
+        }
+        if(!user){
+            return res.status(400).send({code: 400, message: info.message})
+        }
+        return req.login(user,(loginError)=>{
+            if(loginError){
+                console.log(loginError);
+                return next(loginError);
+            }
+            return res.status(200).send({code: 200, data: user});
+            
+        })
+    })(req,res,next);
+})
+
+router.get('/login',(req,res,next)=>{
+    if (req.user) {
+        res.status(200).send({ code: 200, data: req.user });
+    } else {
+        res.status(400).send({ code: 400, error: 'not login' });
+    }
+})
+
+router.get('/logout',(req,res,next)=>{
+    req.logout();
+    req.session.destroy();
+    res.status(200).send({code: 200});
+})
+
+module.exports = router;
---
+++ server/routes/join.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const passport = require('passport');
+const bcrypt = require('bcrypt');
+const router = express.Router();
+
+const User = require('../models/user');
+
+router.post('/id',async(req,res,next)=>{ //id중복확인
+    const exUser = await User.findOne({where:{user_id: req.body.id}});
+    if(!exUser){
+        res.status(200).send({conde: 200, message: '사용가능한 아이디 입니다.'});
+    } else{
+        res.status(400).send({code: 400, message: '이미 사용 중인 아이디입니다.'});
+    }
+})
+
+router.post('/nick',async(req,res,next)=>{ //nickname중복확인
+    const exUser = await User.findOne({where:{nickname: req.body.nickname}});
+    if(!exUser){
+        res.status(200).send({conde: 200, message: '사용가능한 닉네임 입니다.'});
+    } else{
+        res.status(400).send({code: 400, message: '이미 사용 중인 닉네임입니다.'});
+    }
+})
+
+router.post('/',async(req,res,next)=>{
+    try{
+        const exUser = await User.findOne({where:{user_id: req.body.id}});
+        const hash = await bcrypt.hash(req.body.password,12);
+        await User.create({
+            user_id: req.body.id,
+            nickname: req.body.nickname,
+            password: hash,
+        });
+        const user = await User.findOne({user_id: req.body.id});
+        req.login(user,(err)=>{
+            return res.status(200).send({code: 200, ...user});
+        })
+        
+    } catch(error){
+        return next(error);
+    }
+})
+
+module.exports = router;
