42209ad4dd6b7c94935b547fe97c50f33f1a955b
SunEom <dkxl0701@gmail.com>
2021-05-13 16:43:17 +0900

    Updated infowindow UI

--- src/MapContent/MapContentPresenter.jsx
+++ src/MapContent/MapContentPresenter.jsx
@@ -10,10 +10,13 @@ const MapContent = styled.div`
   top: 40px;
 `;
 
+const Test = styled.div`
+  background-color: red;
+`;
+
 const MapContentPresenter = ({ location, restaurantList }) => {
   useEffect(() => {
     const script = document.createElement('script');
-    console.log(restaurantList);
     script.async = true;
     // 지도 API를 위한 script src
     script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.REACT_APP_MAP_API_KEY}&autoload=false`;
@@ -23,6 +26,7 @@ const MapContentPresenter = ({ location, restaurantList }) => {
       kakao.maps.load(() => {
         var positions = restaurantList?.map((restaurant) => {
           return {
+            id: restaurant.id,
             address: restaurant.road_address_name,
             distance: restaurant.distance,
             url: restaurant.place_url,
@@ -54,7 +58,6 @@ const MapContentPresenter = ({ location, restaurantList }) => {
         var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png';
 
         positions?.forEach((p) => {
-          console.log(p);
           // 마커 이미지의 이미지 크기 입니다
           var imageSize = new kakao.maps.Size(24, 35);
 
@@ -70,15 +73,27 @@ const MapContentPresenter = ({ location, restaurantList }) => {
           });
 
           // 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
-          var iwContent = `<div style="padding:5px; width : 200px">
-          <div style="margin-bottom:15px;"> 
-            <a href=${p.url} target="_blank">${p.title}</a>
-          </div>
-          <div style="margin-bottom:15px;">${p.address}</div>
-          <div style="display:flex; justify-content :space-between"> 
-            <div>${p.category}</div>
-            <div>${p.distance}m</div>
-          </div>
+          var iwContent = `<div style="box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px; width : 300px">
+          <img style="width : 100%; height : 150px"src="https://images.unsplash.com/photo-1476224203421-9ac39bcb3327?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGZvb2R8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"/>
+            <div style="padding :9px">
+              <div style="display:flex ; justify-content : space-between; align-items : center;">
+                <a href=${p.url} target="_blank" style="font-size:19px; text-decoration : none; color : black">${
+              p.title.length > 12 ? p.title.slice(0, 12) + '...' : p.title
+            }</a>
+                <div style = "font-size : 13px; margin-right : 5px">${p.category}</div>
+              </div>
+
+              <div style="font-size : 13px;margin-top : 10px;margin-bottom:6px;">${p.address}</div>
+
+              <div style="font-size : 13px"> 
+                <div>${p.distance}m</div>
+              </div>
+
+              <div style="display:flex ;align-items:center; margin-top : 5px">
+                <a href="#">길찾기</a>
+                <form style="margin-left :5px" method="POST"><button type="submit">즐겨찾기 추가</button></form>
+              </div>
+            </div>
           </div>`, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
             iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
