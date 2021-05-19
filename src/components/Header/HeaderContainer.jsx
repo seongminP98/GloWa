701c74d5e384cd0da7a23101e3170aaf3b0fcc9f
SunEom <dkxl0701@gmail.com>
2021-05-19 17:26:51 +0900

    Updated logout process on header

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import store from '../../store';
 import HeaderPresenter from './HeaderPresenter';
@@ -6,7 +7,15 @@ const HeaderContainer = () => {
   const [user, setUser] = useState(store.getState().user);
   const sUser = () => setUser(store.getState().user);
   store.subscribe(sUser);
-  return <HeaderPresenter user={user} />;
+
+  const onLogoutClick = async () => {
+    await axios
+      .get(`${process.env.REACT_APP_SERVER_URL}/auth/logout`, { withCredentials: true })
+      .then(() => store.dispatch({ type: 'LOGOUT' }))
+      .catch((error) => console.error(error));
+  };
+
+  return <HeaderPresenter user={user} onLogoutClick={onLogoutClick} />;
 };
 
 export default HeaderContainer;
