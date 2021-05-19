534b375c5daa35e4610df67cc795b3127f495940
seongmin <psm7335@gmail.com>
2021-05-19 17:38:57 +0900

    modify passport

--- server/passport/index.js
+++ server/passport/index.js
@@ -1,14 +1,16 @@
 const passport = require('passport');
 const local = require('./localStrategy');
 const User = require('../models/user');
-const Schedule  = require('../models/schedule');
 
 module.exports = () => {
+    
     passport.serializeUser((user, done) => {
+        console.log("serial");
         done(null, user.id);
       });
     
     passport.deserializeUser((id, done) => {
+        console.log("deserializeUser");
         User.findOne({where: {id: id},
         include:[{
             model: User,
