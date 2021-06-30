7da35bf83a9fa1acc22c7796d41f1332b7ffe7d0
seongmin <psm7335@gmail.com>
2021-06-30 14:39:52 +0900

    add favorites db

--- server/models/index.js
+++ server/models/index.js
@@ -6,6 +6,7 @@ const User = require('./user');
 const ReqFriend = require('./reqFriends');
 const Schedule = require('./schedule');
 const InvSchedule = require('./invSchedule');
+const Favorites = require('./favorites');
 
 const db={};
 
@@ -20,18 +21,18 @@ db.User = User;
 db.ReqFriend = ReqFriend;
 db.Schedule = Schedule;
 db.InvSchedule = InvSchedule;
-
+db.Favorites = Favorites;
 
 User.init(sequelize);
 ReqFriend.init(sequelize);
 Schedule.init(sequelize);
 InvSchedule.init(sequelize);
-
+Favorites.init(sequelize);
 
 User.associate(db);
 ReqFriend.associate(db);
 Schedule.associate(db);
 InvSchedule.associate(db);
-
+Favorites.associate(db);
 
 module.exports = db;
