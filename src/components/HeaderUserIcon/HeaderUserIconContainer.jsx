2af516a108f1379d94ecf99202c2e9de726dd30b
SunEom <dkxl0701@gmail.com>
2021-07-08 15:06:25 +0900

    Removed profile image process

--- src/components/HeaderUserIcon/HeaderUserIconContainer.jsx
+++ src/components/HeaderUserIcon/HeaderUserIconContainer.jsx
@@ -4,24 +4,7 @@ import HeaderUserIconPresenter from './HeaderUserIconPresenter';
 import store from '../../store';
 
 const HeaderUserIconContainer = (props) => {
-  const { user } = props;
-  const onChange = (e) => {
-    const formData = new FormData();
-    formData.append('img', e.target.files[0]);
-    axios
-      .post(`${process.env.REACT_APP_SERVER_URL}/mypage/image`, formData, {
-        headers: { 'Content-Type': 'multipart/form-data' },
-        withCredentials: true,
-      })
-      .then((response) => {
-        store.dispatch({ type: 'USER_UPDATED', user: { ...user, img: response.data.result } });
-      });
-  };
-  const onImgChangeButtonClick = async () => {
-    const realInput = document.querySelector('#real-input');
-    realInput.click();
-  };
-  return <HeaderUserIconPresenter {...props} onImgChangeButtonClick={onImgChangeButtonClick} onChange={onChange} />;
+  return <HeaderUserIconPresenter {...props} />;
 };
 
 export default HeaderUserIconContainer;
--- src/components/HeaderUserIcon/HeaderUserIconPresenter.jsx
+++ src/components/HeaderUserIcon/HeaderUserIconPresenter.jsx
@@ -1,27 +1,7 @@
 import React from 'react';
-import { ListItemText, Button, Badge, withStyles, Menu, MenuItem, Avatar } from '@material-ui/core';
+import { Avatar } from '@material-ui/core';
 import styled from 'styled-components';
-const HeaderUserIconPresenter = ({ user, onImgChangeButtonClick, onChange }) => {
-  const StyledMenu = withStyles({
-    paper: {
-      border: '1px solid #d3d4d5',
-    },
-  })((props) => (
-    <Menu
-      elevation={0}
-      getContentAnchorEl={null}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'center',
-      }}
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'center',
-      }}
-      {...props}
-    />
-  ));
-
+const HeaderUserIconPresenter = ({ user }) => {
   const ParticipantsList = styled.div`
     margin-left: 5px;
     margin-right: 10px;
@@ -32,53 +12,12 @@ const HeaderUserIconPresenter = ({ user, onImgChangeButtonClick, onChange }) => 
     align-items: center;
   `;
 
-  const StyledMenuItem = withStyles((theme) => ({
-    root: {
-      display: 'flex',
-      alignItems: 'center',
-      '&:hover': {
-        backgroundColor: theme.palette.primary.main,
-        '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
-          color: theme.palette.common.white,
-        },
-      },
-    },
-  }))(MenuItem);
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-  console.log(user);
-  console.log(Image);
-
   return (
     <div>
       <ParticipantsListItemDiv>
         <Avatar style={{ height: 30, width: 30 }}>{user.nickname.slice(0, 1).toUpperCase()}</Avatar>
         <ParticipantsList>{user.nickname}</ParticipantsList>
       </ParticipantsListItemDiv>
-
-      <StyledMenu id="customized-menu" anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
-        <StyledMenuItem className="browse-btn" onClick={onImgChangeButtonClick}>
-          <i className="far fa-image" style={{ marginRight: 10 }}></i>
-          <input
-            style={{ display: 'none' }}
-            type="file"
-            onChange={onChange}
-            id="real-input"
-            class="image_inputType_file"
-            accept="img/*"
-            required
-          ></input>
-          <ListItemText primary="프로필 이미지 수정" />
-        </StyledMenuItem>
-      </StyledMenu>
     </div>
   );
 };
