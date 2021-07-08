7b417245bbcf997d146e19a22690da269025df18
SunEom <dkxl0701@gmail.com>
2021-07-08 16:18:41 +0900

    Re-add profile img change process

--- src/components/HeaderUserIcon/HeaderUserIconContainer.jsx
+++ src/components/HeaderUserIcon/HeaderUserIconContainer.jsx
@@ -4,7 +4,25 @@ import HeaderUserIconPresenter from './HeaderUserIconPresenter';
 import store from '../../store';
 
 const HeaderUserIconContainer = (props) => {
-  return <HeaderUserIconPresenter {...props} />;
+  const { user } = props;
+  console.log(user);
+  const onChange = (e) => {
+    const formData = new FormData();
+    formData.append('img', e.target.files[0]);
+    axios
+      .post(`${process.env.REACT_APP_SERVER_URL}/mypage/image`, formData, {
+        headers: { 'Content-Type': 'multipart/form-data' },
+        withCredentials: true,
+      })
+      .then((response) => {
+        store.dispatch({ type: 'LOGIN', user: { ...user, img: response.data.result } });
+      });
+  };
+  const onImgChangeButtonClick = async () => {
+    const realInput = document.querySelector('#real-input');
+    realInput.click();
+  };
+  return <HeaderUserIconPresenter {...props} onImgChangeButtonClick={onImgChangeButtonClick} onChange={onChange} />;
 };
 
 export default HeaderUserIconContainer;
